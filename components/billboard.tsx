2d7ccdad3dc178315f04712d6cbb6fa397591723
TBTech205 <tylerbeadle658@gmail.com>
2023-04-10 19:32:50 +0100

    Update billboard.tsx
    Made more responsive for phone

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -7,9 +7,9 @@ const Billboard = () => {
 	const { data } = useBillboard()
 
 	return (
-		<div className="relative h-[56.25vw]">
+		<div className="relative h-[60vw] md:h-[40vw] lg:h-[56vw]">
 			<video
-				className="w-full h-[56.25vw] object-cover"
+				className="w-full h-[56vw] object-cover"
 				src={data?.videoUrl}
 				poster={data?.thumbnailUrl}
 				muted
@@ -18,7 +18,7 @@ const Billboard = () => {
 			></video>
 			<div className="absolute text-white top-[30%] ml-4 md:top-[35%] md:ml-16">
 				<h2 className="h-full font-bold drop-shadow-xl text-[15px] md:text-4xl lg:text-6xl">{data?.title}</h2>
-        <p className="text-[17px] mt-3 w-[70%] md:text-[17px] md:w-[50%] lg:w-[80%]">{data?.description}</p>
+        <p className="mt-3 text-[12px] w-[70%] md:text-[17px] md:w-[65%] lg:w-[65%]">{data?.description}</p>
         <div className="flex gap-3">
           <PlayButtonWithText movieId={data?.id} />
           
